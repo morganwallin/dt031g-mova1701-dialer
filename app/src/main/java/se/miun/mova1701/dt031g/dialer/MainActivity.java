665f362e8572c51b73227adcc75d7ea4086f948c
mova1701 <mova1701@student.miun.se>
2019-03-28 14:14:32 +0100

    Added button OnClick for Dial-button, CallList-button and Settings-button

--- app/src/main/java/se/miun/mova1701/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/mova1701/dt031g/dialer/MainActivity.java
@@ -1,7 +1,11 @@
 package se.miun.mova1701.dt031g.dialer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button dialButton = (Button) findViewById(R.id.dialButton);
+        dialButton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, DialActivity.class));
+            }
+        });
+
+        Button callListButton = (Button) findViewById(R.id.callListButton);
+        callListButton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, CallListActivity.class));
+            }
+        });
+
+        Button settingsButton = (Button) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+
+
     }
 }
