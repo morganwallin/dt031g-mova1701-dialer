8e07c2ce30bb90a828398c7da8d21aeb83d74269
mova1701 <mova1701@student.miun.se>
2019-03-28 14:57:03 +0100

    Added AlertDialog for About-button

--- app/src/main/java/se/miun/mova1701/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/mova1701/dt031g/dialer/MainActivity.java
@@ -1,11 +1,14 @@
 package se.miun.mova1701.dt031g.dialer;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.app.AlertDialog.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
         mapButton.setOnClickListener(new OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+
+        Button aboutButton = (Button) findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.dialog_message) .setTitle(R.string.dialog_title);
+                builder.setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
 
